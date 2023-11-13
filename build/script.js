b4bfa7b6e15d8d4835a8efb03a52a2eb1d802c87
Cole Winchester <cowinch@gmail.com>
2023-11-13 14:11:08 -0600

    initial commit

---
+++ build/script.js
@@ -0,0 +1,12 @@
+let total = 10
+let apples = 0,
+    chickens = 0,
+    cheese = 0,
+    bread = 0,
+    spice = 0,
+    silk = 0,
+    alcohol = 0,
+    crossbow = 0
+
+document.getElementById('totalPoints').innerHTML = total
+    
